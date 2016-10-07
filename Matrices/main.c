7c9d33362580164dc5c0ef25c545550dac839980
Alexander Bradley <lex@Alexanders-MacBook-Pro.local>
2016-10-07 11:34:16 -0500

    Base

--- Matrices/main.c
+++ Matrices/main.c
@@ -7,9 +7,46 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    srand(time(NULL));
+    char size = ( rand() % 10) + 1, matriz[size][size], entrantes[size], salientes[size];
+    int counter = 0, i, j;
+    
+    for ( i = 0; i < size; i++){
+        entrantes[i] = 0;
+        salientes[i] = 0;
+        for (int j = 0; j < size; j++) {
+            matriz[i][j] = rand() % 2;
+        }
+    }
+    
+    printf("La matriz es de %d X %d\n\n", size, size);
+    
+    for ( i = 0; i < size; i++){
+        if (matriz[i][i]){
+            counter+=1;
+        }
+        printf("|");
+        for ( j = 0; j < size; j++) {
+            printf(" %d |",matriz[i][j]);
+            if (matriz[i][j]){
+                salientes[i]+=1;
+                entrantes[j]+=1;
+            }
+        }
+        printf("\n\n");
+    }
+    
+    printf("El numero de lazos es de %d\n\n", counter);
+    
+    for (i = 0; i < size; i++){
+        printf("El nodo %d tiene:\n", i);
+        printf("%d entrantes\n", entrantes[i]);
+        printf("%d salientes\n", salientes[i]);
+    }
+    
     return 0;
 }
