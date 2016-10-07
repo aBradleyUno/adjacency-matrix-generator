270f6772a36c25ac559edc4d00c71f474ed1134c
Alexander Bradley <alexandro20.0@icloud.com>
2016-10-07 10:41:07 -0500

    Initial Commit

---
+++ Matrices/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Matrices
+//
+//  Created by Alexander Bradley on 10/7/16.
+//  Copyright © 2016 Ingenieria de Software UAZ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
